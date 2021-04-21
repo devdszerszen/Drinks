cce92e89d3ea2ef57308a431bfb2622300ccbe9a
Damian Szerszen <dev.dszerszen@gmail.com>
2021-04-21 20:26:04 +0200

    Initial Commit

---
+++ Drinks/DrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinksApp.swift
+//  Drinks
+//
+//  Created by Damian Szerszeń on 21/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
